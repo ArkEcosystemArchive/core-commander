ee1b5e3e28225de7ebb74e8f99a5270245c0957b
Rob <vRobM@users.noreply.github.com>
2018-06-13 15:10:59 -0700

    Update help.sh
    Native English corrections

--- menus/main/help.sh
+++ menus/main/help.sh
@@ -5,43 +5,43 @@ menu_main_help ()
     ascii
 
     text_yellow "    Install ARK Core (I)"
-    text_white "    opens ARK relay node process options."
+    text_white "    starts the ARK core intall process."
     echo
 
     text_yellow "    Manage ARK Core (A)"
-    text_white "    opens ARK core options."
+    text_white "    opens the ARK core options."
     echo
 
     text_yellow "    Manage Relay (R)"
-    text_white "    opens ARK relay node process options."
+    text_white "    opens the ARK relay node process options."
     echo
 
     text_yellow "    Manage Forger (F)"
-    text_white "    opens ARK forger node process options."
+    text_white "    opens the ARK forger node process options."
     echo
 
     text_yellow "    Manage Explorer (E)"
-    text_white "    opens ARK explorer options."
+    text_white "    opens the ARK explorer options."
     echo
 
     text_yellow "    Manage Commander (C)"
-    text_white "    opens ARK commander options."
+    text_white "    opens the ARK commander options."
     echo
 
     text_yellow "    Miscellaneous (M)"
-    text_white "    opens miscellaneous menu."
+    text_white "    opens the miscellaneous menu."
     echo
 
-    text_yellow "    Show Log (L)"
-    text_white "    opens core log so you can see what is going on with your node process in real-time."
+    text_yellow "    Show Logs (L)"
+    text_white "    opens the Ark core logs so you can see what is going on with your node process in real-time."
     echo
 
     text_yellow "    Show Process Monitor (P)"
-    text_white "    opens PM2 monitor tool so you can see log information where app is, CPU and memory usage and much more."
+    text_white "    opens the PM2 monitor tool so you can see log information where the app is running, CPU, memory usage and much more."
     echo
 
     text_yellow "    Show Help (H)"
-    text_white "    opens help file where all this information is available."
+    text_white "    opens the help file where all this information is available."
     echo
 
     text_yellow "    X — Exit"
@@ -49,7 +49,7 @@ menu_main_help ()
 
     divider
 
-    text_blue "    For more information head over to https://arkdocs.readme.io/"
+    text_blue "    For more information head on over to https://arkdocs.readme.io/"
     echo
 
     press_to_continue
