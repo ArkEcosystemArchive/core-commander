045b54a3c3fc5f390abc18e467e4b1b3577b2a30
Rob <vRobM@users.noreply.github.com>
2018-06-13 15:14:25 -0700

    Update help.sh
    Native English corrections

--- menus/relay/help.sh
+++ menus/relay/help.sh
@@ -7,27 +7,27 @@ menu_manage_relay_help ()
     divider
 
     text_yellow "    Stop Relay (K)"
-    text_white "    stops ARK relay node process (shown only if relay node is running)."
+    text_white "    stops the ARK relay node process (shown only if the relay node is running)."
     echo
 
     text_yellow "    Start Relay (S)"
-    text_white "    starts ARK relay node process (shown only if relay node is not running)."
+    text_white "    starts the ARK relay node process (shown only if the relay node is not running)."
     echo
 
     text_yellow "    Restart Relay (R)"
-    text_white "    restarts (stops and starts) relay node process."
+    text_white "    restarts (stops and then starts) the relay node process."
     echo
 
-    text_yellow "    Show Log (L)"
-    text_white "    shows PM2 relay node process log."
+    text_yellow "    Show Logs (L)"
+    text_white "    shows the PM2 relay node process logs."
     echo
 
     text_yellow "    Show Help (H)"
-    text_white "    opens help file where all this information is available."
+    text_white "    opens the help file where all this information is available."
 
     divider
 
-    text_blue "    For more information head over to https://arkdocs.readme.io/"
+    text_blue "    For more information head on over to https://arkdocs.readme.io/"
     echo
 
     press_to_continue
