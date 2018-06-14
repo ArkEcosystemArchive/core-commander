f280759dee2114c6b07d23c1b2cc9fdd723f6bd8
Brian Faust <hello@brianfaust.me>
2018-06-14 06:00:30 +0300

    refeactor: install node from source

--- scripts/nodejs.sh
+++ scripts/nodejs.sh
@@ -4,14 +4,12 @@ nodejs_install ()
 {
     heading "Installing node.js & npm..."
 
-    (curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash) >> "$commander_log" 2>&1
+    sudo rm -rf /usr/local/{lib/node{,/.npm,_modules},bin,share/man}/{npm*,node*,man1/node*}
+    sudo rm -rf ~/{.npm,.forever,.node*,.cache,.nvm}
 
-    export NVM_DIR="${HOME}/.nvm"
-    [ -s "${NVM_DIR}/nvm.sh" ] && . "${NVM_DIR}/nvm.sh"
-
-    nvm install 9.11.1 >> "$commander_log" 2>&1
-    nvm use 9.11.1 >> "$commander_log" 2>&1
-    nvm alias default 9.11.1 >> "$commander_log" 2>&1
+    (echo "deb https://deb.nodesource.com/node_9.x $(lsb_release -s -c) main" | sudo tee /etc/apt/sources.list.d/nodesource.list) >> "$commander_log" 2>&1
+    sudo apt-get update >> "$commander_log" 2>&1
+    sudo apt-get install nodejs -y --allow-unauthenticated >> "$commander_log" 2>&1
 
     success "Installed node.js & npm!"
 }
