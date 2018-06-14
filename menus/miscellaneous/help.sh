a4a8f5d48b2db17749ab490c845faa4023030f45
Rob <vRobM@users.noreply.github.com>
2018-06-13 21:28:30 -0700

    Update help.sh (#10)
    Native English corrections

--- menus/miscellaneous/help.sh
+++ menus/miscellaneous/help.sh
@@ -9,19 +9,19 @@ menu_miscellaneous_help ()
     echo
 
     text_yellow "    Create Commander Executable (E)"
-    text_white "    lets you create executable so you can do “./commander.sh” instead of “bash ./commander.sh” to start commander."
+    text_white "    lets you create an executable so you can do “./commander.sh” instead of “bash ./commander.sh” to start the Commander."
     echo
 
     text_yellow "    Create Commander Alias (A)"
-    text_white "    lets you create alias so you can start Commander by inputting “commander” instead of “bash ./commander.sh”."
+    text_white "    lets you create an alias so you can start Commander by inputting “commander” instead of “bash ./commander.sh”."
     echo
 
     text_yellow "    Show Help (H)"
-    text_white "    opens help file where all this information is available."
+    text_white "    opens the help file where all this information is available."
 
     divider
 
-    text_blue "    For more information head over to https://arkdocs.readme.io/"
+    text_blue "    For more information head on over to https://arkdocs.readme.io/"
     echo
 
     press_to_continue
