d08220f39f445cc66504dfb6f02cbc19e33dc9a3
Brian Faust <hello@brianfaust.me>
2018-06-15 06:50:29 +0300

    fix: disable the default systemd timesyncd service

--- scripts/ntp.sh
+++ scripts/ntp.sh
@@ -44,6 +44,7 @@ ntp_install ()
         if [[ "$STATUS_NTP" = "Off" ]]; then
             heading "Installing NTP..."
 
+            sudo timedatectl set-ntp off # disable the default systemd timesyncd service
             sudo apt-get install ntp -yyq | tee -a "$commander_log"
             ntp_stop
             sudo ntpd -gq | tee -a "$commander_log"
