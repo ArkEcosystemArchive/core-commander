d6880fb0dab2f963bb49d143230a70fcb3cb2373
Brian Faust <envoyer@pm.me>
2018-08-15 10:49:06 +0300

    fix: checkout in core directory

--- actions/core/install.sh
+++ actions/core/install.sh
@@ -13,8 +13,8 @@ core_install ()
         sudo chown "$USER":"$USER" "$CORE_DIR" >> "$commander_log" 2>&1
         sudo rm -rf "$CORE_DIR" >> "$commander_log" 2>&1
         git clone "$CORE_REPO" "$CORE_DIR" | tee -a "$commander_log"
-        git checkout develop | tee -a "$commander_log"
         cd "$CORE_DIR"
+        git checkout develop | tee -a "$commander_log"
         lerna bootstrap | tee -a "$commander_log"
 
         core_configure
