8b63dec900b9f9ac7aa78d292dc1505493c2dccd
Edgar Goetzendorff <hello@dated.fun>
2018-10-21 00:58:58 +0200

    fix: check if logfile exists before tailing

--- actions/log.sh
+++ actions/log.sh
@@ -3,9 +3,30 @@
 utils_log ()
 {
     clear
-    echo -e "\n$(text_yellow " Use Ctrl+C to return to menu")\n"
-    trap : INT
 
-    local log_file=$(ls -t "${CORE_DATA}/logs/core/${CORE_NETWORK}/" | head -n 1)
-    tail -fn 50 "${CORE_DATA}/logs/core/${CORE_NETWORK}/${log_file}"
+    local log_dir="${CORE_DATA}/logs/core/${CORE_NETWORK}/"
+
+    if [ ! -d "$log_dir" ]; then
+        ascii
+
+        error "Log folder does not exist."
+
+        return 1
+    fi
+
+    local log_file=$(ls -t "${log_dir}" | head -n 1)
+
+    if [ ! -e "${log_dir}/${log_file}" ]; then
+        ascii
+
+        error "Log file does not exist."
+
+        return 1
+    else
+        echo -e "\n$(text_yellow " Use Ctrl+C to return to menu")\n"
+
+        trap : INT
+
+        tail -fn 50 "${log_dir}/${log_file}"
+    fi
 }
