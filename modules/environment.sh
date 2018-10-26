b39318c4a7bce9efbf7e15ca716139a8214ff697
Georgi Stoyanov <33185290+geops22@users.noreply.github.com>
2018-10-25 21:07:52 -0500

    Use only system user as psql user.

--- modules/environment.sh
+++ modules/environment.sh
@@ -11,7 +11,7 @@ setup_environment_file ()
 
         echo "ARK_DB_HOST=localhost" >> "$envFile" 2>&1
         echo "ARK_DB_PORT=5432" >> "$envFile" 2>&1
-        echo "ARK_DB_USERNAME=ark" >> "$envFile" 2>&1
+        echo "ARK_DB_USERNAME=${USER}" >> "$envFile" 2>&1
         echo "ARK_DB_PASSWORD=password" >> "$envFile" 2>&1
         echo "ARK_DB_DATABASE=ark_devnet" >> "$envFile" 2>&1
     fi
