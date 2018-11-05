ab8b776ca1be1941d89754aa38f26d50fc1b7b15
dated <hello@dated.fun>
2018-11-05 12:40:55 +0100

    chore: add log level config to core_configure

--- actions/core/configure.sh
+++ actions/core/configure.sh
@@ -18,6 +18,8 @@ core_configure ()
 
             core_configure_database
 
+            core_configure_log_level
+
             __core_configure_post
 
             configured=true
@@ -30,6 +32,8 @@ core_configure ()
         __core_configure_network
 
         core_configure_database
+
+        core_configure_log_level
 
         __core_configure_post
 
