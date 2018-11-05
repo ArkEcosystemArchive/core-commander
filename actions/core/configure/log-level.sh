f210dac42b85487ab2e69cd4877ba55e8ef4a3bb
dated <hello@dated.fun>
2018-11-05 12:11:14 +0100

    refactor: use select for log level

--- actions/core/configure/log-level.sh
+++ actions/core/configure/log-level.sh
@@ -2,15 +2,41 @@
 
 core_configure_log_level ()
 {
+    ascii
+
     local envFile="${CORE_DATA}/.env"
 
     . "$envFile"
 
-    read -p "Enter the log level (debug, info, warning, error), or press ENTER for the default [$ARK_LOG_LEVEL]: " inputLevel
+    info "Which log level would you like to configure?"
 
-    if [[ ! -z "$inputLevel" ]]; then
-        sed -i -e "s/ARK_LOG_LEVEL=$ARK_LOG_LEVEL/ARK_LOG_LEVEL=$inputLevel/g" "$envFile"
-    fi
+    validLevels=("debug" "info" "warning" "error")
+
+    select opt in "${validLevels[@]}"; do
+        case "$opt" in
+            "debug")
+                local inputLevel=debug
+                break
+            ;;
+            "info")
+                local inputLevel=info
+                break
+            ;;
+            "warning")
+                local inputLevel=warning
+                break
+            ;;
+            "error")
+                local inputLevel=error
+                break
+            ;;
+            *)
+                echo "Invalid option $REPLY"
+            ;;
+        esac
+    done
+
+    sed -i -e "s/ARK_LOG_LEVEL=$ARK_LOG_LEVEL/ARK_LOG_LEVEL=$inputLevel/g" "$envFile"
 
     . "$envFile"
 }
