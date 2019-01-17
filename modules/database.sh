66805d3b6873c29f7a8c91f23e259650e23c2003
Edgar Goetzendorff <hello@dated.fun>
2019-01-17 10:46:27 +0100

    fix: use env variables for database creation (#109)

--- modules/database.sh
+++ modules/database.sh
@@ -4,7 +4,7 @@ database_drop_user ()
 {
     heading "Dropping Database User..."
 
-    sudo -u postgres dropuser --if-exists "$ARK_DB_USERNAME" | tee -a "$commander_log"
+    sudo -u postgres dropuser --if-exists "${ARK_DB_USERNAME}" | tee -a "$commander_log"
 
     success "Dropped Database User!"
 }
@@ -13,7 +13,7 @@ database_destroy ()
 {
     heading "Destroying Database..."
 
-    sudo -u postgres dropdb --if-exists "$ARK_DB_DATABASE" | tee -a "$commander_log"
+    sudo -u postgres dropdb --if-exists "${ARK_DB_DATABASE}" | tee -a "$commander_log"
 
     success "Destroyed Database!"
 }
@@ -26,17 +26,17 @@ database_create ()
 
     wait_to_continue
 
-    local userExists=$(sudo -u postgres psql -c "SELECT * FROM pg_user WHERE usename = '$USER'" | grep -c "1 row")
+    local userExists=$(sudo -u postgres psql -c "SELECT * FROM pg_user WHERE username = '${ARK_DB_USERNAME}'" | grep -c "1 row")
 
     if [[ $userExists == 1 ]]; then
-        read -p "The database user ${USER} already exists, do you want to overwrite it? [y/N] : " choice
+        read -p "The database user ${ARK_DB_USERNAME} already exists, do you want to overwrite it? [y/N] : " choice
 
         if [[ "$choice" =~ ^(yes|y|Y) ]]; then
-            sudo -u postgres psql -c "DROP USER $USER" | tee -a "$commander_log"
-            sudo -u postgres psql -c "CREATE USER $USER WITH PASSWORD 'password' CREATEDB;" | tee -a "$commander_log"
+            sudo -u postgres psql -c "DROP USER ${ARK_DB_USERNAME}" | tee -a "$commander_log"
+            sudo -u postgres psql -c "CREATE USER ${ARK_DB_USERNAME} WITH PASSWORD '${ARK_DB_PASSWORD}' CREATEDB;" | tee -a "$commander_log"
         fi
     else
-        sudo -u postgres psql -c "CREATE USER $USER WITH PASSWORD 'password' CREATEDB;" | tee -a "$commander_log"
+        sudo -u postgres psql -c "CREATE USER ${ARK_DB_USERNAME} WITH PASSWORD '${ARK_DB_PASSWORD}' CREATEDB;" | tee -a "$commander_log"
     fi
 
     local databaseExists=$(psql -l | grep "${ARK_DB_DATABASE}" | wc -l)
